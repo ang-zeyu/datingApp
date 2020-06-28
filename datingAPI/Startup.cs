e91c33072fbba315c409f87b279f852cfa6175a9
Ze Yu <angzeyu@gmail.com>
2020-06-28 15:17:45 +0800

    Remove useHttpsRedirection

--- datingAPI/Startup.cs
+++ datingAPI/Startup.cs
@@ -36,7 +36,7 @@ namespace datingAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
