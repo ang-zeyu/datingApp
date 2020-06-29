3ffd5c18d3a6935ac3bbeef13e900e07dbe59f29
Ze Yu <angzeyu@gmail.com>
2020-06-29 14:47:58 +0800

    Bootstrap SPA to display values

--- datingSPA/src/app/app.module.ts
+++ datingSPA/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ValuesComponent } from './values/values.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ValuesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
