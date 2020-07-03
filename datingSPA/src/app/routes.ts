c2d77383cfa0148729f74ec8ba7f4a42718710f2
Ze Yu <angzeyu@gmail.com>
2020-07-03 14:29:09 +0800

    Cleanup routes.ts

--- datingSPA/src/app/routes.ts
+++ datingSPA/src/app/routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent} from './home/home.component';
-import { MembersComponent } from './members/members.component';
+import { MembersComponent } from './members/member-list/members.component';
 import { FavouritesComponent } from './favourites/favourites.component';
 import { MessagesComponent } from './messages/messages.component';
 
