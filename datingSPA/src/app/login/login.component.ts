e636fa0d4604e9cfedca8f10eda4ba5c49fd5780
Ze Yu <angzeyu@gmail.com>
2020-07-03 12:55:51 +0800

    Cleanup login component styles

--- datingSPA/src/app/login/login.component.ts
+++ datingSPA/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Output, EventEmitter, Input} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth/auth.service';
 import alertify from 'alertifyjs';
 
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   username: string;
